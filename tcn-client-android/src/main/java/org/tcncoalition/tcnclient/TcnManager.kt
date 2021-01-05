961cf36cf83b58cef7a33b6d421b41afb08b050a
ismael alvarez <ismaelalvarez@MacBook-Pro-de-ismael.local>
2021-01-04 21:32:18 -0300

    add channel notification

--- tcn-client-android/src/main/java/org/tcncoalition/tcnclient/TcnManager.kt
+++ tcn-client-android/src/main/java/org/tcncoalition/tcnclient/TcnManager.kt
@@ -1,11 +1,17 @@
 package org.tcncoalition.tcnclient
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 import org.tcncoalition.tcnclient.bluetooth.BluetoothStateListener
 import org.tcncoalition.tcnclient.bluetooth.TcnBluetoothService
 import org.tcncoalition.tcnclient.bluetooth.TcnBluetoothServiceCallback
@@ -22,7 +28,7 @@ abstract class TcnManager(
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             this@TcnManager.service =
                 (service as TcnBluetoothService.LocalBinder).service.apply {
-                    val notification = foregroundNotification().build()
+                    val notification = createNotification()
                     startForegroundNotificationIfNeeded(NOTIFICATION_ID, notification)
                     setBluetoothStateListener(this@TcnManager)
                     startTcnExchange(this@TcnManager)
@@ -36,6 +42,36 @@ abstract class TcnManager(
     }
 
     abstract fun foregroundNotification(): NotificationCompat.Builder
+
+    private fun createNotification(): Notification {
+
+        // depending on the Android API that we're dealing with we will have
+        // to use a specific method to create the notification
+        val notificationChannelId = "happ_notification"
+
+        // depending on the Android API that we're dealing with we will have
+        // to use a specific method to create the notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                notificationChannelId,
+                "notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder: Notification.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) Notification.Builder(
+            context,
+            notificationChannelId
+        ) else Notification.Builder(context)
+
+
+        return builder
+            .setContentTitle("Happ se está ejecutando")
+            .setPriority(Notification.PRIORITY_HIGH) // for under android 26 compatibility
+            .build()
+    }
 
     fun startService() {
         context.bindService(
